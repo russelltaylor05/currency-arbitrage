00b6156a90fa9f39fb861ae48d845cab4e373b0c
russell <you@example.com>
2016-03-23 16:15:10 -0700

    initial

---
+++ index.js
@@ -0,0 +1,120 @@
+var Bluebird      = require('bluebird');
+var request       = Bluebird.promisify(require('request'));
+
+var URL = 'http://fx.priceonomics.com/v1/rates/';
+
+
+/* 
+ * RUN
+ */ 
+request({ url: URL, method: "GET" })
+.then(function (response) {  
+  
+  var matrix    = createMatrix(JSON.parse(response.body));  
+  var start     = kickOff(matrix);
+  var results   = traverse(matrix, start);  
+  var best      = results[0];
+
+  console.log('All possibilities: \n');
+  results.forEach(function (node, index) {    
+    console.log('Path: ' +  node.path + '\t\t Arbitrage: ' + node.arbValue);
+    if (node.arbValue > best.arbValue) {
+      best = node;
+    }    
+  });
+  console.log('\n\nBest Path: ' +  best.path + '->' + best.first);
+  console.log('Arbitrage Value: ' +  best.arbValue);
+});
+
+
+/* 
+ * Transform JSON data into multi-dim array notation
+ * EX: matrix['USD']['JPY'] = 0.88
+ */ 
+var createMatrix = function (data) {
+  var matrix = {}; 
+  var curFrom, curTo;
+  for (i in data) {
+    curFrom = i.match(/^[\w]{3}/, i)[0];
+    curTo = i.match(/[\w]{3}$/, i)[0];        
+    if (!matrix[curFrom]) {
+      matrix[curFrom] = {};
+    }
+    matrix[curFrom][curTo] = data[i];    
+  }
+  return matrix;
+}
+
+/* 
+ * Takes our currency matrix and calculates all arbitrage possibilities of length 1 
+ * EX: (USD -> EUR -> USD)
+ */
+var kickOff = function (matrix) {
+
+  var results = [];
+
+  for (var i in matrix) {    
+    for (var j in matrix[i]) {    
+      if ( i != j) {
+        
+        var remaining = JSON.parse(JSON.stringify(matrix[i]));
+        delete remaining[i];
+        delete remaining[j];
+        
+        results.push({
+          path: i + '->' + j,       
+          value: matrix[i][j],        
+          arbValue: matrix[i][j] * matrix[j][i],
+          remaining: remaining,
+          first: i,
+          last: j
+        });
+      }
+    }
+  }
+  return results;
+}
+
+/* 
+ * Recursivly find all arbitrage paths from remaining currencies
+ */
+var traverse = function (matrix, results) {
+
+  var newResults = [];
+
+  for (var i in results) {
+    for (var j in results[i].remaining) {
+
+      var remaining = JSON.parse(JSON.stringify(results[i].remaining));        
+      delete remaining[j];
+
+      var object = {
+        path: results[i].path + '->' + j, 
+        value: matrix[results[i].last][j] * results[i].value,
+        remaining: remaining,
+        first: results[i].first,
+        last: j
+      };
+      object.arbValue = object.value * matrix[object.last][object.first];        
+      newResults.push(object);
+    
+    }
+  }
+  return (newResults.length) ? results.concat(traverse(matrix, newResults)) : results;
+}
+
+
+/****************************
+All Arbitrage possibilities starting with a single currency
+
+               USD
+            /   |   \
+         /      |      \      
+      /         |         \
+   EUR         BTC         JPY
+  /   \       /   \      /    \
+BTC   JPY   EUR   JPY   BTC   EUR
+ |     |     |     |     |     |
+JPY   BTC   JPY   EUR   EUR   BTC
+ 
+****************************/
